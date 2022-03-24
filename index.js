4345f15b81ae1d7fc4e0ef60e5c1f39ab349bbfe
carsonlafave <clafaveaz@gmail.com>
2022-03-24 13:09:02 -0700

    commit tree c84c1351b35f6a5ae8eab322465a87d1d846aa70

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2;
 
-// Write your code here
+
+ const random = Math.ceil( '10' )
+ 
+ const num3 = 100;
+ const num4 = 6;
+
+ const mod = num3 % num4;
+
+ const max = Math.max(1,2,5,10,20)
